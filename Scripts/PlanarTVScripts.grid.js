4ebb2304a53a6a08ab8bee0832f44acac9fe3565
sailingbo <bocordle@sailingbo.com>
2019-10-31 11:31:57 -0400

    Made tv.__() module for all the TV commands.

--- Scripts/PlanarTVScripts.grid.js
+++ Scripts/PlanarTVScripts.grid.js
@@ -1,22 +1,21 @@
-// This is the Ross Callback Function
-function callback(success, sentData, resultString, exception) {
-  ogscript.debug("Success: " + success);
-  ogscript.debug("Sent Data: " + sentData);
-  ogscript.debug("Result String: " + resultString);
-  ogscript.debug("Exception: " + exception);
-  if (!success) {
-    ogscript.debug("Unable to send message: " + exception);
-  } else {
-    // ogscript.debug("Result String via Callback: " + resultString);
-    var hexString = convert.toHexString(resultString);
-    var percentageVolume = resultString[8].toString(10);
-    // ogscript.debug("Resulting Hex String: " + hexString);
-    // ogscript.debug("Volume Percentage: " + percentageVolume + "%");
-    return resultString;
-  }
-}
+/**
+ * This creates a tv module that sends commands to Planar 4k Simplicity Series TVs
+ * tv.getVolume(tvIPAddress, listener)
+ * tv.setVolume(volumeLevel as INT, tvIPAddress, listener)
+ * tv.SendToTV(command, tvIPAddress, listener)
+ * tv.getChecksum(hexString) returns hexstring + checksum at the end.
+ *
+ * The responses from the TV are handled in the Dashboard Panel's listener function.
+ *
+ * These commands work for Planar 4k Simplicity Series TVs.
+ * https://www.planar.com/media/437583/020-1344-00a_planar-simplicity-series-rs232-user-guide.pdf
+ *
+ * bocordle@sailingbo.com 10/31/2019
+ */
 
-// This function takes a volume INT and converts it to a string to be sent to the TV
+var tv = (function() {
+
+// This function gets the current TV volume.
 function getVolume(tvIPAddress, listener) {
   logFile.write("Get Volume Button Pushed.");
   var getVolumeCommand = "A6 01 00 00 00 03 01 45 E0";
@@ -40,7 +39,28 @@ function setVolume(volumeLevel, tvIPAddress, listener) {
   sendToTV(setVolumeCommand, tvIPAddress, listener);
 }
 
-// This function sends a command to the TV.
+// This function gets the power state of the TV
+function getPowerState(tvIPAddress, listener) {
+  var getPowerCommand = "A6 01 00 00 00 03 01 19 BC";
+  // Send command to TV now.
+  sendToTV(getPowerCommand, tvIPAddress, listener);
+}
+
+// This function turns on the TV
+function turnOn(tvIPAddress, listener) {
+  var setPowerCommand = "A6 01 00 00 00 04 01 18 02 BB";
+  // Send command to TV now.
+  sendToTV(setPowerCommand, tvIPAddress, listener);
+}
+
+// This function turns on the TV
+function turnOff(tvIPAddress, listener) {
+  var setPowerCommand = "A6 01 00 00 00 04 01 18 01 BB";
+  // Send command to TV now.
+  sendToTV(setPowerCommand, tvIPAddress, listener);
+}
+
+// This function sends a passed command to the TV.
 function sendToTV(command, tvIPAddress, listener) {
   var listener = ogscript.getObject(listener);
   if (listener != null){
@@ -48,13 +68,13 @@ function sendToTV(command, tvIPAddress, listener) {
     listener.writeAsBytes(command);
   }
   else {
-    logFile.write("No listener connection found. Maybe try sendAsBytes?");
+    logFile.write("No listener connection found. Try sendAsBytes?");
     // rosstalk.sendAsBytes(tvIPAddress, 5000, command, callback);
   }
 }
 
-// This function calculates the proper checksum for Planar TVs
-function LRC(hexstring) {
+// This function calculates the proper checksum (LRC) for Planar TVs
+function getChecksum(hexstring) {
   if (hexstring != "") {
     var s = hexstring.match(/../g);
     var lrc = 0;
@@ -71,3 +91,14 @@ function LRC(hexstring) {
     ogscript.debug("Hexstring is null.");
   }
 }
+
+return {
+  getVolume: getVolume,
+  setVolume: setVolume,
+  getPowerState: getPowerState,
+  turnOn: turnOn,
+  turnOff: turnOff,
+  sendToTV: sendToTV,
+  getChecksum: getChecksum
+}
+}());
